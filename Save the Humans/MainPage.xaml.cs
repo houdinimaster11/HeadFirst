3715bcbb047c3468e534b4b5beeda5339a4ea77b
Evan Hilberg <evanhilberg@gmail.com>
2014-01-29 17:38:27 -0800

    initial

--- Save the Humans/MainPage.xaml.cs
+++ Save the Humans/MainPage.xaml.cs
@@ -112,27 +112,29 @@ namespace Save_the_Humans
 
  private void AddEnemy()
         {
+                
             ContentControl enemy = new ContentControl();
             enemy.Template = Resources["EnemyTemplate"] as ControlTemplate;
-            AnimateEnemy(enemy, 0, playArea.ActualWidth - 100, "(Canvas.Left)");
-            AnimateEnemy(enemy, random.Next((int)playArea.ActualHeight - 100),
-                random.Next((int)playArea.ActualHeight - 100), "(Canvas.Top)");
+            AnimateEnemy(enemy, 0, playArea.ActualWidth - 25, new PropertyPath("(Canvas.Left)"));
+            AnimateEnemy(enemy, random.Next((int)playArea.ActualHeight - 25), random.Next((int)playArea.ActualHeight - 25), new PropertyPath("(Canvas.Top)"));
             playArea.Children.Add(enemy);
+
+            //enemy.PointerEntered += enemy_PointerEntered;
         }
 
- private void AnimateEnemy(ContentControl enemy, double from, double to, string propertyToAnimate)
- {
-     Storyboard storyboard = new Storyboard() { AutoReverse = true, RepeatBehavior = RepeatBehavior.Forever };
-     DoubleAnimation animation = new DoubleAnimation()
-     {
-         From = from,
-         To = to,
-         Duration = new Duration(TimeSpan.FromSeconds(random.Next(4, 6)))
-     };
-     Storyboard.SetTarget(animation, enemy);
-     Storyboard.SetTargetProperty(animation, propertyToAnimate);
-     storyboard.Children.Add(animation);
-     storyboard.Begin();
+        private void AnimateEnemy(ContentControl enemy, double from, double to, PropertyPath propertyPath)
+        {
+            Storyboard storyboard = new Storyboard() { AutoReverse = true, RepeatBehavior = RepeatBehavior.Forever };
+            DoubleAnimation animation = new DoubleAnimation()
+            {
+                From = from,
+                To = to,
+                Duration = new Duration(TimeSpan.FromSeconds(random.Next(4, 6)))
+            };
+            Storyboard.SetTarget(animation, enemy);
+            Storyboard.SetTargetProperty(animation,propertyPath);
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
         }
     }
 }
